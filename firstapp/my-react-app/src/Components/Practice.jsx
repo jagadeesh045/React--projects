de15391ffecb77ea2da70c1e1d0b9ba072a820b1
jagadeesh045 <127129689+jagadeesh045@users.noreply.github.com>
2025-02-16 16:28:13 +0530

    add button

---
+++ firstapp/my-react-app/src/Components/Practice.jsx
@@ -0,0 +1,13 @@
+
+
+const Practice = () => {
+  return (
+    <div>
+        <h1>Practice</h1>
+        <input type="text" />
+        <button>Click Me</button>
+    </div>
+  )
+}
+
+export default Practice
